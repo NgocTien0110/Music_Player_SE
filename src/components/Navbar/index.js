13564b89369c0ade61f17183a7999846f88dff94
Tuong <lengoctuong23052002@gmail.com>
2022-08-04 18:18:26 +0700

    add NavArrows into Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import NavLinks from "./NavLinks";
+import NavArrows from './NavArrows';
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
     return (
         <div className="flex justify-between items-center bg-[color:var(--color-navbar-bg)] h-16 px-[10vw] inset-0 fixed z-50 backdrop-blur-[20px] backdrop-saturate-[180%]">
+            {/* Navigation Buttons Arrow */}
+            <NavArrows />
+            {/* End Navigation Buttons Arrow */}
+
             {/* Navigation Links */}
             <div className="flex">
                 <NavLinks toLink="/" titleLink="Home" />
                 <NavLinks toLink="/zingchart" titleLink="ZingChart" />
                 <NavLinks toLink="/top100" titleLink="Top 100" />
-
-
             </div>
         </div>
     );
