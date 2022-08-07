565d3e0ff3c9128494605cac93467874c3e19097
ntp-shin <tanphatnguyen2002@gmail.com>
2022-08-07 15:44:35 +0700

    Fix bug redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,12 +6,5 @@ const store = configureStore({
         audio: audioReducer,
     },
 });
-// Ê, mi biết mấy file ni có chức năng chi không ?
-//
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
 
-// export { store }
 export default store;
