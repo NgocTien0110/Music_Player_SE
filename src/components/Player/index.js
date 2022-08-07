a5387a7799e78e8f1fe8edb63fdcfc7ecce399bf
ntp-shin <tanphatnguyen2002@gmail.com>
2022-08-07 15:50:28 +0700

    UseSelector to get songID, lyric,... in file index - Player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,123 +1,102 @@
-import React, { useEffect, useRef } from "react"
-import Controls from "./Control"
-import { getSong, getInfoSong } from "../../api/song"
-import { useAppSelector, useAppDispatch } from "../../hooks/redux"
+import React, { useEffect, useRef } from 'react';
+import Controls from './Control';
+import { getSong, getInfoSong } from '../../api/song';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  setInfoSongPlayer,
-  setCurrentTime,
-  setDuration,
-  setSrcAudio,
-  changeIconPlay,
-} from "../../redux/features/audioSlice"
-import { setSongId, setCurrnetIndexPlaylist } from "../../redux/features/audioSlice"
-import Lyric from "./Lyric"
+    setInfoSongPlayer,
+    setCurrentTime,
+    setDuration,
+    setSrcAudio,
+    changeIconPlay,
+} from '../../redux/features/audioSlice';
+import { setSongId, setCurrentIndexPlaylist } from '../../redux/features/audioSlice';
+import Lyric from './Lyric';
 
 const Player = () => {
+    const songId = useSelector((state) => state.audio.songId);
+    const srcAudio = useSelector((state) => state.audio.srcAudio);
+    const isLoop = useSelector((state) => state.audio.isLoop);
+    const isLyric = useSelector((state) => state.audio.isLyric);
+    const dispath = useDispatch();
 
-  const songId = useAppSelector((state) => state.audio.songId)
-  const srcAudio = useAppSelector((state) => state.audio.srcAudio)
-  const isLoop = useAppSelector((state) => state.audio.isLoop)
-  const isLyric = useAppSelector((state) => state.audio.isLyric)
-  const dispath = useAppDispatch()
+    const currnetIndexPlaylist = useSelector((state) => state.audio.currnetIndexPlaylist);
+    const playlistSong = useSelector((state) => state.audio.playlistSong);
 
-  const currnetIndexPlaylist = useAppSelector((state) => state.audio.currnetIndexPlaylist)
-  const playlistSong = useAppSelector((state) => state.audio.playlistSong)
+    const dispatch = useDispatch();
 
-  const dispatch = useAppDispatch()
+    const audioRef = useRef();
 
-  const audioRef = useRef<HTMLAudioElement | null>(null)
-
-  useEffect(() => {
-    (
-      async () => {
-        if(songId !== null && songId !== "") {
-          const src = await getSong(songId)
-          src[128] ? dispath(setSrcAudio( src[128] )) : dispath(setSrcAudio(""))
-          const infoSong = await getInfoSong(songId)
-          dispath(setInfoSongPlayer(
-            {
-              title: infoSong.title,
-              thumbnail: infoSong.thumbnail,
-              artistsNames: infoSong.artistsNames,
+    useEffect(() => {
+        (async () => {
+            if (songId !== null && songId !== '') {
+                const src = await getSong(songId);
+                src[128] ? dispath(setSrcAudio(src[128])) : dispath(setSrcAudio(''));
+                const infoSong = await getInfoSong(songId);
+                dispath(
+                    setInfoSongPlayer({
+                        title: infoSong.title,
+                        thumbnail: infoSong.thumbnail,
+                        artistsNames: infoSong.artistsNames,
+                    }),
+                );
             }
-          ))
-        }
-      }
-    )()
-  }, [songId, dispath])
+        })();
+    }, [songId, dispath]);
 
-  return (
-    <>
-        {
-          songId
-          ?
-          <div className="flex flex-col justify-around h-16 backdrop-saturate-[180%] backdrop-blur-[30px] bg-[color:var(--color-navbar-bg)] fixed inset-x-0 bottom-0 z-[100]">
-            <Controls auRef={audioRef.current} />
-          </div>
-          :
-          ""
-        }
+    return (
+        <>
+            {songId ? (
+                <div className="flex flex-col justify-around h-16 backdrop-saturate-[180%] backdrop-blur-[30px] bg-[color:var(--color-navbar-bg)] fixed inset-x-0 bottom-0 z-[100]">
+                    <Controls auRef={audioRef.current} />
+                </div>
+            ) : (
+                ''
+            )}
 
-      <audio
-        ref={audioRef}
-        src={srcAudio}
-        className="hidden"
-        loop={isLoop}
-        autoPlay={true}
-        hidden
-        onTimeUpdate = {() => {
-            if(audioRef.current) {
-              dispath(setCurrentTime(
-                (audioRef.current.currentTime)
-              ))
-            }
-          }
-        }
-        onLoadedData = {() => {
-            if(audioRef.current) {
-              dispath(setDuration(
-                (audioRef.current.duration)
-              ))
-            }
-        }}
-        onEnded = {() => {
-          if (!isLoop) {
-            dispath(setCurrentTime(0))
-            dispath(changeIconPlay(false))
+            <audio
+                ref={audioRef}
+                src={srcAudio}
+                className="hidden"
+                loop={isLoop}
+                autoPlay={true}
+                hidden
+                onTimeUpdate={() => {
+                    if (audioRef.current) {
+                        dispath(setCurrentTime(audioRef.current.currentTime));
+                    }
+                }}
+                onLoadedData={() => {
+                    if (audioRef.current) {
+                        dispath(setDuration(audioRef.current.duration));
+                    }
+                }}
+                onEnded={() => {
+                    if (!isLoop) {
+                        dispath(setCurrentTime(0));
+                        dispath(changeIconPlay(false));
 
-            if(playlistSong !== undefined && playlistSong.length > 0) {
+                        if (playlistSong !== undefined && playlistSong.length > 0) {
+                            let currentIndex;
 
-              let currentIndex
+                            if (currnetIndexPlaylist === playlistSong.length - 1) {
+                                currentIndex = 0;
+                            } else {
+                                currentIndex = currnetIndexPlaylist + 1;
+                            }
 
-              if(currnetIndexPlaylist === playlistSong.length - 1) {
-                currentIndex = 0
-              } else {
-                currentIndex = currnetIndexPlaylist + 1
-              }
+                            dispatch(setCurrentIndexPlaylist(currentIndex));
 
-              dispatch(setCurrnetIndexPlaylist(
-                currentIndex
-              ))
+                            dispatch(setSongId(playlistSong[currentIndex].encodeId));
 
-              dispatch(setSongId(
-                playlistSong[currentIndex].encodeId
-              ))
+                            dispatch(changeIconPlay(true));
+                        }
+                    }
+                }}
+            />
 
-              dispatch(changeIconPlay(true))
-            }
+            {isLyric ? <Lyric /> : ''}
+        </>
+    );
+};
 
-          }
-        }}
-      />
-
-      {
-      isLyric
-      ? <Lyric />
-      : ""
-      }
-
-    </>
-  )
-}
-
-export default Player
+export default Player;
